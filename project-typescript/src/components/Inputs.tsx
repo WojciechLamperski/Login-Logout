a6f669c282b970fb6f2347108f0f921066e72aea
Wojciech Lamperski <wolamp20@gmail.com>
2022-10-25 09:23:31 +0200

    Bug found in inputs.tsx

--- project-typescript/src/components/Inputs.tsx
+++ project-typescript/src/components/Inputs.tsx
@@ -5,34 +5,30 @@ import { StyledInputs } from "./styles/Inputs.styled";
 export const Inputs:any = ():any => {
     const [login, setLogin] = useState<string>('');
     const [password, setPassword] = useState('');
-    // const [localObject, setLocalObject] = useState({});
-    
-    // type localObject = {
-    //     login: string;
-    //     password: string;
-    // }
 
+    // Ta funkcja jest przypisana do formularza, dlatego nie działa,
+    // bo jesteśmy w inputs, które nie mają dostępu do "form"
+    function handleSubmit(e : any) : void {
+        e.preventDefault();
 
+        const obj = {name: login, password: password};
+        console.log(obj);
+        const myJSON = JSON.stringify(obj);
 
-function handleSubmit(e : any) : void {
-    e.preventDefault();
-    // setLocalObject({login : password});
+        localStorage.setItem('user', myJSON);
+    } 
+    // BUG! ^
 
-    const obj = {name: login, password: password};
-    const myJSON = JSON.stringify(obj);
+    function handleChangeLogin(event:any){
+        setLogin(event.target.value);
+        console.log(event.target.value);
+    }
+        
+    function handleChangePassword(event:any){
+        setPassword(event.target.value);
+        console.log(event.target.value);
+    }
 
-    localStorage.setItem('user', myJSON);
-}
-
-function handleChangeLogin(event:any){
-    setLogin(event.target.value);
-    console.log(event.target.value);
-}
-     
-function handleChangePassword(event:any){
-    setPassword(event.target.value);
-    console.log(event.target.value);
-}
     return(
         <>
             <StyledInputs onChange={handleChangeLogin} placeholder="login" type="text"></StyledInputs>
